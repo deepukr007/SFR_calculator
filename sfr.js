88518c703d6e1a3bfd9359d33d8f1f387918c0b3
unknown <deepukreddy123@gmail.com>
2021-04-16 00:43:52 +0530

    Initial Commit

---
+++ sfr.js
@@ -0,0 +1,23 @@
+function submit()
+{
+s_no = document.getElementById('students_no').value
+f_no = document.getElementById('faculty_no').value
+
+s_no = parseFloat(s_no)
+f_no = parseFloat(f_no)
+
+if(s_no == 0 || f_no ==0)
+{
+	alert("Zero is not allowed");
+}
+
+else
+{
+	sfr = s_no / f_no ;
+	document.getElementById('result').innerHTML = sfr.toFixed(2);
+}
+
+
+
+
+}
